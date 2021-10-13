ba32dc9c8397fccbf96139ecfff5cc12255c736e
SquidGeneral10 <SquidGeneral10GameDev@Gmail.com>
2021-10-13 10:28:46 +0100

    Level transitions, starting a main menu
    Also opened in Unity version 2019.3 and then returned to 2020 version.

--- Bippity Boppity BOOM/Assets/Scripts/ThirdPersonCharacter/ThirdPersonCamera.cs
+++ Bippity Boppity BOOM/Assets/Scripts/ThirdPersonCharacter/ThirdPersonCamera.cs
@@ -8,6 +8,8 @@ public class ThirdPersonCamera : MonoBehaviour
 {
     [SerializeField] private Transform player, centrePoint;
     private float mouseX, mouseY;
+    [SerializeField] private float rAnalogSens = 0f; // Lets you change the joystick sensitivity.
+    [SerializeField] private bool invertVertical = false; // Allows for inverted camera options, if the player is a twisted individual.
 
     void Start() // Start is called before the first frame update
     {
@@ -27,8 +29,29 @@ public class ThirdPersonCamera : MonoBehaviour
     {
         mouseX += Input.GetAxis("Mouse X");
         mouseY -= Input.GetAxis("Mouse Y");
+
+        if (Input.GetJoystickNames().Length > 0)
+        {
+            mouseX += Input.GetAxis("RightVertical") * rAnalogSens;
+
+            if (!invertVertical)
+            {
+                mouseY += Input.GetAxis("RightHorizontal") * rAnalogSens;
+            }
+            else
+            {
+                mouseY -= Input.GetAxis("RightHorizontal") * rAnalogSens;
+            }
+        }
+        else
+        {
+            mouseX += Input.GetAxis("Mouse X");
+            mouseX -= Input.GetAxis("Mouse Y");
+        }
+
         mouseY = Mathf.Clamp(mouseY, -60f, 60f);
 
+        transform.rotation = Quaternion.Euler(mouseY, mouseX, 0f);
         centrePoint.localRotation = Quaternion.Euler(mouseY, mouseX, 0);
     }
 }
